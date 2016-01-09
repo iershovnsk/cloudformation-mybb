c556c0245d7508105574d353580f36f4e5726b84
Valeriu Paloş <me@vpalos.com>
2016-01-09 04:23:42 +0200

    Switched to Amazon Linux AMI.

--- Source/install.sh
+++ Source/install.sh
@@ -5,8 +5,7 @@
 # (C) Valeriu Paloş <valeriupalos@gmail.com>
 #
 # Apache2, Php5x and required dependencies for MyBB are expected to be
-# proertly added to the system by this point. This installation script
-# was tested on an Ubuntu AMI. Invoke from **this** working directory!
+# properly added to the system by this point.
 #
 
 # Environment variables (expected).
