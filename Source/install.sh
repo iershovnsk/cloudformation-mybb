ca59f009622af61b9f55e6a572048eb699386841
iershovnsk <iershov.nsk@gmail.com>
2017-05-09 17:56:02 +0700

    making some fixes in install.sh

--- Source/install.sh
+++ Source/install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 #
 # MyBB AMI installation script invoked by AWS CloudFormation template.
@@ -57,7 +57,9 @@ chmod 666 inc/config.php inc/settings.php
 chmod 666 inc/languages/english/*.php inc/languages/english/admin/*.php
 
 # TODO: The "uploads/" path should be mounted on an S3 bucket.
-mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 ${MYBB_FILESYSTEM}.efs.${MYBB_REGION}.amazonaws.com:/ uploads
-chown www-data:www-data uploads/
+mkdir -p ${TARGET}/uploads
+mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 ${MYBB_FILESYSTEM}.efs.${MYBB_REGION}.amazonaws.com:/ ${TARGET}/uploads
+#chown  uploads/
+mkdir -p ${TARGET}/uploads/avatars
 chmod 777 cache/ cache/themes/ uploads/ uploads/avatars/
 chmod 777 cache/ cache/themes/ uploads/ uploads/avatars/ admin/backups/
